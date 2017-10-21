6d6ee38e2e325c1ae5f5b5f5c07728ffb0aa24d4
Gabrielle <galaxygab21@gmail.com>
2017-10-21 17:46:28 +0000

    use .each do block on index page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,35 @@
 class TodoController < ApplicationController
-    def index
-    end
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find_by_id(params[:id])
+  end
+  
+  def create
+    t = Todo.new
+    t.description = params['description']
+    t.pomodoro_estimate = params['pomodoro-estimate']
+    t.save
+    redirect_to "/todo/show/#{ t.id }"
+  end
+  
+  def edit
+    @todo = Todo.find_by_id(params[:id])
+  end
+  
+  def update
+    t = Todo.find_by_id(params[:id])
+    t.description = params['description']
+    t.pomodoro_estimate = params['pomodoro-estimate']
+    t.save
+    redirect_to "/todo/show/#{t.id}"
+  end
+  
+  def destroy
+    t = Todo.find_by_id(params[:id])
+    t.destroy
+    redirect_to "/"
+  end
 end
